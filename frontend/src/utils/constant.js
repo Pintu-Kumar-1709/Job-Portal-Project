fc2677036d5f62c0c1b181de92d32533135f61e4
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2025-12-29 19:33:13 +0530

    url changed

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,10 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ;
 
-export const JOB_API_END_POINT = "http://localhost:8080/api/v1/job";
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
 
-export const APPLICATION_API_END_POINT = "http://localhost:8080/api/v1/application";
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
 
-export const COMPANY_API_END_POINT = "http://localhost:8080/api/v1/company";
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
 
