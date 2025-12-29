bf67fe8c576efc4896bfb7bc1273060f720ea084
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2025-12-29 18:19:32 +0530

    final repo!

--- backend/index.js
+++ backend/index.js
@@ -7,22 +7,25 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicantRoute from "./routes/application.route.js";
+import path from "path";
 dotenv.config({});
 
 const app = express();
+const __dirname = path.resolve();
 
-app.get("/home", (req, res) => {
-  return res.status(200).json({
-    message: "Helowh from backend",
-    success: true,
-  });
-});
+// app.get("/home", (req, res) => {
+//   return res.status(200).json({
+//     message: "Hello from backend",
+//     success: true,
+//   });
+// });
 
 //middlewares
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOptions = {
   origin: "http://localhost:5173",
   credentials: true,
@@ -30,13 +33,17 @@ const corsOptions = {
 app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
-//api's             
+//api's
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicantRoute);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on ${PORT}`);
