608b6d3a5528a043b48c54eb9bc9ee1c9de7b6a3
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-04-10 18:21:58 +0530

    update application controller

--- backend/controller/application.controller.js
+++ backend/controller/application.controller.js
@@ -11,7 +11,7 @@ export const applyJob = async (req, res) => {
         success: false,
       });
     }
-    //check if user has already applied for the job
+  
     const existingApplication = await Application.findOne({
       job: jobId,
       applicant: userId,
@@ -22,7 +22,7 @@ export const applyJob = async (req, res) => {
         success: false,
       });
     }
-    //check if job exists or not
+    
     const job = await Job.findById(jobId);
     if (!job) {
       return res.status(404).json({
@@ -30,7 +30,7 @@ export const applyJob = async (req, res) => {
         success: false,
       });
     }
-    //create a new application
+   
     const newApplication = await Application.create({
       job: jobId,
       applicant: userId,
@@ -74,7 +74,7 @@ export const getAppliedJobs = async (req, res) => {
   }
 };
 
-//for admin to see how many applicants applied for a job
+
 export const getApplicants = async (req, res) => {
   try {
     const jobId = req.params.id;
@@ -112,7 +112,7 @@ export const updateStatus = async (req, res) => {
         success: false,
       });
     }
-    //find application by application id
+   
     const application = await Application.findOne({ _id: applicationId });
     if (!application) {
       return res.status(404).json({
@@ -120,7 +120,7 @@ export const updateStatus = async (req, res) => {
         success: false,
       });
     }
-    //update status
+ 
     application.status = status;
     await application.save();
     return res.status(200).json({
